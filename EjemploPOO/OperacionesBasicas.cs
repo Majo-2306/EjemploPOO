0105c26e4406dd74ec663ac1512d40c86fa2f06d
Majo-2306 <maria_castro24@unikino.edu.mx>
2024-11-21 07:53:12 -0700

    Agregar archivos de proyecto.

---
+++ EjemploPOO/OperacionesBasicas.cs
@@ -0,0 +1,25 @@
+﻿namespace EjemploPOO
+{
+    internal class OperacionesBasicas
+    {
+        public int Sumar(int primerNumero, int segundoNumero)
+        {
+            return primerNumero + segundoNumero;
+        }
+
+        public int Resta(int primerNumero, int segundoNumero)
+        {
+            return primerNumero - segundoNumero;
+        }
+
+        public int Multiplicacion(int primerNumero, int segundoNumero)
+        {
+            return primerNumero * segundoNumero;
+        }
+
+        public int Dividir(int primerNumero, int segundoNumero)
+        {
+            return primerNumero / segundoNumero; 
+        }
+    }
+}
---
+++ EjemploPOO/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace EjemploPOO
+{
+    internal class Program
+    {
+        //EL VOID ES UN MÉTODO
+        static void Main(string[] args)
+        {
+            //Instancias, esto crea un nuevo objeto
+            OperacionesBasicas basicas = new OperacionesBasicas();
+
+            int resultadoSuma = basicas.Sumar(5, 5);
+            Console.WriteLine(resultadoSuma);
+
+            int otraSuma = basicas.Sumar(14, 8);
+            Console.WriteLine(otraSuma);
+
+
+
+        }
+    }
+}
